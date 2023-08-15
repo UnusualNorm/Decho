f3a28aad171b9ebfe3b871d2368b4a5382326056
Luka Grenon <62908057+MathieDev@users.noreply.github.com>
2023-08-15 15:06:35 -0400

    Add files via upload

---
+++ utils/logging.ts
@@ -0,0 +1,14 @@
+import fs from 'node:fs';
+
+export function log(event: string) {
+    const logFilePath = '../events.log';
+    const logMessage = `${new Date().toISOString()} - ${event}\n`;
+  
+    fs.appendFile(logFilePath, logMessage, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(event);
+      }
+    });
+  }
