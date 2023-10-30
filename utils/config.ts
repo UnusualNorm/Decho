008189ac8fc38c5f403f1a896c644f7d04c2fef0
Unusual norm <40722525+UnusualNorm@users.noreply.github.com>
2023-10-29 22:42:05 -0500

    Fix configs

--- utils/config.ts
+++ utils/config.ts
@@ -43,9 +43,14 @@ export const typeIntToString = (type: bigint): string | undefined =>
 
 export const idStringToIntMap: [string, bigint][] = [
   ["main_menu", 0x7b1d0e4427ee0915n],
+
   ["battle_pass_season_00_invalid", 0x4a2443d62ce5c6c7n],
   ["store_entry_0036", 0x12a0d011f264c4c1n],
   ["store_featured_entry_0013", 0xedc05dddbd8cb455n],
+
+  ["active_battle_pass_season", 0x7ec7455095134e79n],
+  ["active_store_entry", 0xc92d42ecab56db59n],
+  ["active_store_featured_entry", 0x31f9d212b1224955n],
 ];
 
 export const idStringToInt = (id: string): bigint | undefined =>
